c0aaba30c4108123dd49503047250b95075d8abd
Elias Junior <elijah.junnior@outlook.com>
2021-07-04 11:00:18 -0300

    creating route to test

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,20 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
+
 
 export const router = Router();
+
+type Message = {
+    texto: string;
+}
+
+function CriaMensagm(request: Request, response: Response) {
+
+    const msg = <Message>{
+        texto: "Teste de mensagem"
+    }
+
+    return response.json(msg);
+
+}
+
+router.get('/teste', CriaMensagm);
